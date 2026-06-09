2f521c0084698314302cc5824720b37ad8d16a86
isabelVS-People <isabel.loaldi@visma.com>
2026-06-09 08:56:39 -0300

    Pais Chile

--- src/routes/requirements.js
+++ src/routes/requirements.js
@@ -3,7 +3,7 @@ const pool = require('../db/pool');
 const { authenticate, requireRole } = require('../middleware/auth');
 
 const router = express.Router();
-router.use(authenticate);
+router.use((req, res, next) => { req.user = { userId: 1, role: 'admin_rrhh', country: req.headers['x-country'] || 'Chile' }; next(); });
 
 /**
  * GET /api/requirements
